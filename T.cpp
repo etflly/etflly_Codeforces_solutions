d8a4a8c93b0c8c874fc87a880eccb83c1f2c6564
etflly <etflly@gmail.com>
2016-01-17 11:30:52 +0800

    模板

---
+++ T.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+const int INF = 1e9;
+const long long INFL = 1e18;
+const double EP = 1e-9;
+const double PI = 3.141592653589793284626433832795;
+const int MP = 1e9+7;
+
+void init()
+{
+}
+
+void solve()
+{
+}
+
+void print()
+{
+}
+
+int main()
+{
+#ifndef ONLINE_JUDGE
+    freopen("A.in", "r", stdin);
+    //freopen("A.out", "w", stdout);
+#endif
+    init();
+    solve();
+    print();
+}
